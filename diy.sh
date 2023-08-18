8bf06583c5ac2c9356c0bc3c167b13ee97c6bd0f
najloa <jecrdhs@gmail.com>
2023-08-18 23:16:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
