2e6efe508ffbc8ffd2e0937fa07a9b0e75ce1a9c
najloa <jecrdhs@gmail.com>
2023-08-26 09:30:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
