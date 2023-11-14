28e533f71f81d01d3aab43b1e8bf310e17a2c71c
najloa <jecrdhs@gmail.com>
2023-11-14 12:21:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-sed -i 's/mt7981-fw-20230306/mt7981-fw-20230717/g' package/mtk/drivers/mt_wifi/Makefile
-sed -i 's/mt7981-fw-20230411/mt7981-fw-20230717/g' package/mtk/drivers/warp/Makefile
+#sed -i 's/mt7981-fw-20230306/mt7981-fw-20230717/g' package/mtk/drivers/mt_wifi/Makefile
+#sed -i 's/mt7981-fw-20230411/mt7981-fw-20230717/g' package/mtk/drivers/warp/Makefile
