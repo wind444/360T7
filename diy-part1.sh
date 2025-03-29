c1e1d02d0afd7af70d42a063fb4f3c2409fd9930
najloa <jecrdhs@gmail.com>
2025-03-30 02:16:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 # Add a feed source
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #git clone https://github.com/kenzok8/small-package.git package/small-package && mv ./package/small-package/homebox package/homebox && rm -rf ./package/small-package
 #git clone https://github.com/Black-Steel/openwrt-packages-20231128.git package/openwrt-packages-20231128 && mv ./package/openwrt-packages-20231128/luci-app-homebox package/luci-app-homebox && rm -rf ./package/openwrt-packages-20231128
