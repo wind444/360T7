e7a9e5ee5769da4cca675dafa2d768774ee2fbc5
najloa <jecrdhs@gmail.com>
2025-04-10 15:43:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,7 +7,7 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
+# find . -type d -name "passwall"
 sed -i 's/192.168.1.1/192.168.3.1/' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt-2.4G/NW2.4G/' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -17,4 +17,13 @@ sed -i 's/encryption=none/encryption=sae-mixed/' package/mtk/applications/mtwifi
 sed -i '/encryption=sae-mixed/a \ \ \ \ set wireless.default_${dev}.key=blue1235' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+rm -rf ./feeds/luci/applications/luci-app-passwall && cp -r ./package/passwall/luci-app-passwall ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/packages/net/chinadns-ng && cp -r ./package/passwall-packages/chinadns-ng ./feeds/packages/net/chinadns-ng
+rm -rf ./feeds/packages/net/dns2socks && cp -r ./package/passwall-packages/dns2socks ./feeds/packages/net/dns2socks
+rm -rf ./feeds/packages/net/ipt2socks && cp -r ./package/passwall-packages/ipt2socks ./feeds/packages/net/ipt2socks
+rm -rf ./feeds/packages/net/microsocks && cp -r ./package/passwall-packages/microsocks ./feeds/packages/net/microsocks
+rm -rf ./feeds/packages/net/tcping && cp -r ./package/passwall-packages/tcping ./feeds/packages/net/tcping
+rm -rf ./feeds/packages/net/xray-core && cp -r ./package/passwall-packages/xray-core ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/v2ray-geodata && cp -r ./package/passwall-packages/v2ray-geodata ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/v2ray-plugin && cp -r ./package/passwall-packages/v2ray-plugin ./feeds/packages/net/v2ray-plugin
 git clone https://github.com/immortalwrt/packages.git immortalwrt-packages && rm -rf ./feeds/packages/lang/golang && mv ./immortalwrt-packages/lang/golang feeds/packages/lang/golang && rm -rf ./immortalwrt-packages
