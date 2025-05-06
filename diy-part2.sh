57ac27bc8913766a08bb344e0ad63fe48a39fd5b
najloa <jecrdhs@gmail.com>
2025-05-06 16:41:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,6 +36,24 @@ rm -rf ./feeds/packages/net/simple-obfs && cp -r ./package/passwall-packages/sim
 rm -rf ./feeds/packages/net/trojan-plus && cp -r ./package/passwall-packages/trojan-plus ./feeds/packages/net/trojan-plus
 rm -rf ./feeds/packages/net/tuic-client && cp -r ./package/passwall-packages/tuic-client ./feeds/packages/net/tuic-client
 rm -rf ./feeds/packages/net/xray-plugin && cp -r ./package/passwall-packages/xray-plugin ./feeds/packages/net/xray-plugin
+# 修改 ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+sed -i 's|https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest|https://api.github.com/repos/najloa/geoip/releases/latest|g' ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+sed -i 's|translate("Loyalsoldier/geosite")|translate("najloa/geosite")|g' ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+sed -i 's|translate("Loyalsoldier/geoip")|translate("najloa/geoip")|g' ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+sed -i 's|translate("Loyalsoldier/v2ray-rules-dat")|translate("najloa/v2ray-rules-dat")|g' ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+sed -i 's|https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt|https://raw.githubusercontent.com/najloa/geoip/rules/proxy-list.txt|g' ./feeds/luci/applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule.lua
+# 修改 ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/0_default_config
+sed -i 's|https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest|https://api.github.com/repos/najloa/geoip/releases/latest|g' ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/0_default_config
+sed -i 's|https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt|https://raw.githubusercontent.com/najloa/geoip/rules/proxy-list.txt|g' ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/0_default_config
+# 修改 ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rule_update.lua
+sed -i 's|https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest|https://api.github.com/repos/najloa/geoip/releases/latest|g' ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rule_update.lua
+sed -i 's|https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt|https://raw.githubusercontent.com/najloa/geoip/rules/proxy-list.txt|g' ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rule_update.lua
+sed -i 's|local excluded_domain = {[^}]*}|local excluded_domain = {}|g' ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rule_update.lua
+curl -s https://core.telegram.org/resources/cidr.txt | perl -ne 'print "$_\n" if /^((\d{1,3}\.){3}\d{1,3}\/\d+|([0-9a-fA-F]{1,4}:){2,7}[0-9a-fA-F]{1,4}\/\d{1,3})$/g' > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/proxy_ip
+: > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/chnlist
+: > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+: > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+: > ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/direct_ip
 cp -r ./package/passwall-packages/geoview ./feeds/packages/net/geoview
 # sed -i '/PKG_BUILD_DEPENDS:=/s/$/ upx\/host/' feeds/packages/net/geoview/Makefile
 # sed -i '/\$(INSTALL_DIR) $(1)\/usr\/bin/a \ \tupx --best --lzma $(PKG_INSTALL_DIR)/usr/bin/geoview' feeds/packages/net/geoview/Makefile
